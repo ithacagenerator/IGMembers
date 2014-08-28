1393475c05b3e285998bd4fe017e23aaa8a4ef02
vicatcu <victor.aprea@wickeddevice.com>
2014-08-27 22:14:47 -0400

    failing test for missing route users/invoices

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe MembershipsController, :type => :controller do
+describe MembershipsController, :type => :controller do
+  # This should return the minimal set of attributes required to create a valid
+  # MembershipType. As you add validations to MembershipType, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) { { "name" => "MyString", monthlycost: 5 } }
+
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # MembershipTypesController. Be sure to keep this updated too.
+  let(:valid_session) { {} }
+
+  describe "GET users/invoices?month=x&year=y" do
+    it "assigns the requested membership_type as @membership_type" do
+      get 'users/invoices', {:month => 3, :year => 2014}, valid_session
+      expect(response.status).to eq(200)
+    end
+  end
 
 end
