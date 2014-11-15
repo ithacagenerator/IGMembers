041b4f32f662098287313a42c487b2cf4d895f30
Buddha Buck <blaisepascal@gmail.com>
2014-11-14 21:55:59 -0500

    Undo last change

--- db/migrate/20141106014631_remove_unuseful_fields_from_users.rb
+++ db/migrate/20141106014631_remove_unuseful_fields_from_users.rb
@@ -5,7 +5,7 @@ class RemoveUnusefulFieldsFromUsers < ActiveRecord::Migration
     remove_column :users, :city, :string
     remove_column :users, :state, :string
     remove_column :users, :zip, :string
-    remove_column :users, :membership_type_id, :integer
+    remove_column :users, :membership_type, :string
     remove_column :users, :membership_date, :string
   end
 end
