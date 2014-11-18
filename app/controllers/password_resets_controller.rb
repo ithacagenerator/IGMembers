668bfa1cd2b482defad702584992437c4f141955
Buddha Buck <blaisepascal@gmail.com>
2014-11-17 21:39:35 -0500

    Add password reset edit/update actions.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,6 +10,22 @@ class PasswordResetsController < ApplicationController
     user.send_password_reset if user
     redirect_to root_url, notice: 'Email has been sent with reset instructions'
   end
+
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
+  end
+
+  def update
+    p = params.require(:user).permit(:password, :password_confirmation)
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => 'Password &crarr; reset has expired.'
+    elsif @user.update_attributes(p)
+      redirect_to root_url, :notice => 'Password has been reset.'
+    else
+      render :edit
+    end
+  end
 end
 
 
